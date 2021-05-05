634bea53ce06cbb6facc535353ba41479a29c29c
lilySisouvong <50887253+lilySisouvong@users.noreply.github.com>
2021-05-05 18:38:42 -0400

    Update main.py
    Here's the most recent change to main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import final
 import comp_two
 import streamlit as st 
+from PIL import Image
+
 
 PAGES = {
     "Main Page": final,
     "Two Comparisons": comp_two
 }
+
+img = Image.open('AC_icon.png')
+
+st.set_page_config(
+    page_title='Animal Crossing Ro.',
+    page_icon = img,
+    initial_sidebar_state="expanded",
+)
 
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
